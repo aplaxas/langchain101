8ef1e31a1b09e0acedc520197c2480816ff13033
Jongmin Lee <jongmin.lee@pyanllc.com>
2023-08-31 22:23:52 -0500

    Clean

--- chatbot_llama/checkllama.py
+++ chatbot_llama/checkllama.py
@@ -65,19 +65,3 @@ llm = HuggingFaceHub(
 llm_chain = LLMChain(prompt=prompt, llm=llm)
 
 print(llm_chain.run(text))
-
-
-# question = "Who won the FIFA World Cup in the year 1994? "
-# template = """Question: {question}
-# Answer: Let's think step by step."""
-
-# prompt = PromptTemplate(template=template, input_variables=["question"])
-
-# repo_id = "meta-llama/Llama-2-7b-chat-hf"
-
-# llm = HuggingFaceHub(
-#     repo_id=repo_id, model_kwargs={"temperature": 0.5, "max_length": 64}
-# )
-# llm_chain = LLMChain(prompt=prompt, llm=llm)
-
-# print(llm_chain.run(question))
