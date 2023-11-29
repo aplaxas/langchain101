b7787b3fccb9c4eed81ac5c7f91eb8238db2584e
aplaxas <aplaxas@gmail.com>
2023-11-28 19:12:42 -0600

    use os env

--- chatbot_assistant/app.py
+++ chatbot_assistant/app.py
@@ -5,16 +5,13 @@ import time
 import os
 import json
 from dotenv import load_dotenv
-# load_dotenv()
 
 # Set your OpenAI Assistant ID here
-assistant_id = 'asst_eEyOliJvBPuwW13QfBaVeFaI'  #os.getenv('ASSISTANT_ID')
+os.environ['OPENAI_API_KEY'] = st.secrets['OPENAI_API_KEY']
+os.environ['ASSISTANT_ID'] = st.secrets['ASSISTANT_ID']
 
 # Initialize the OpenAI client (ensure to set your API key in the sidebar within the app)
-client = openai(
-    api_key='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',
-)
-
+client = openai
 
 # Initialize session state variables for file IDs and chat control
 if "file_id_list" not in st.session_state:
@@ -31,13 +28,13 @@ st.set_page_config(page_title="ESG GRI Advisor", page_icon=":moneybag:", layout=
 st.header(":moneybag: Smart Investment Advisor")
 
 #Get the OPENAI API Key
-openai_api_key_env = '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' # os.getenv('OPENAI_API_KEY')
+openai_api_key_env = os.environ['OPENAI_API_KEY']
 openai_api_key = st.sidebar.text_input(
     'OpenAI API Key', placeholder='sk-', value=openai_api_key_env)
 url = "https://platform.openai.com/account/api-keys"
 st.sidebar.markdown("Get an Open AI Access Key [here](%s). " % url)
 if openai_api_key:
-    openai.api_key = 'XXXXXX' ###openai_api_key
+    openai.api_key = openai_api_key
 
 # Button to start the chat session
 if st.sidebar.button("Start Chat"):
